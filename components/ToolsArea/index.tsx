b0ca3587443f1b908662dd62015a9af8f9421d2d
a2tane <a2tane.hrt@gmail.com>
2022-09-26 00:15:07 +0900

    仮のイメージ画像の出力の実装

--- components/ToolsArea/index.tsx
+++ components/ToolsArea/index.tsx
@@ -1,15 +1,36 @@
 import { Download } from "@mui/icons-material";
 import { IconButton } from "@mui/material";
 
+// let canvas = document.createElement("canvas");
+// const ctx = canvas.getContext("2d");
+// if (ctx != null) {
+//   const imageData = ctx.createImageData(100, 100);
+
+//   // Fill the array with RGBA values
+//   for (let i = 0; i < imageData.data.length; i += 4) {
+//     // Percentage in the x direction, times 255
+//     let x = ((i % 400) / 400) * 255;
+//     // Percentage in the y direction, times 255
+//     let y = (Math.ceil(i / 400) / 100) * 255;
+
+//     // Modify pixel data
+//     imageData.data[i + 0] = x; // R value
+//     imageData.data[i + 1] = y; // G value
+//     imageData.data[i + 2] = 255 - x; // B value
+//     imageData.data[i + 3] = 255; // A value
+//   }
+
+//   // Draw image data to the canvas
+//   ctx.putImageData(imageData, 20, 20);
+// }
+
 export const ToolsArea = () => {
-  const handleDownload = () => {
-    const text = "TEXT";
-    const blob = new Blob([text], { type: "text/plain" });
+  const downloadImage = (blob: any) => {
     const url = URL.createObjectURL(blob);
 
     const anchor = document.createElement("a");
     anchor.setAttribute("href", url);
-    anchor.setAttribute("download", "FILENAME.txt");
+    anchor.setAttribute("download", "dot-icon.png");
 
     const mouseEvent = new MouseEvent("click", {
       bubbles: true,
@@ -19,6 +40,34 @@ export const ToolsArea = () => {
 
     anchor.dispatchEvent(mouseEvent);
     console.log("click");
+  };
+
+  const handleDownload = () => {
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    if (ctx != null) {
+      const imageData = ctx.createImageData(100, 100);
+
+      // Fill the array with RGBA values
+      for (let i = 0; i < imageData.data.length; i += 4) {
+        // Percentage in the x direction, times 255
+        let x = ((i % 400) / 400) * 255;
+        // Percentage in the y direction, times 255
+        let y = (Math.ceil(i / 400) / 100) * 255;
+
+        // Modify pixel data
+        imageData.data[i + 0] = x; // R value
+        imageData.data[i + 1] = y; // G value
+        imageData.data[i + 2] = 255 - x; // B value
+        imageData.data[i + 3] = 255; // A value
+      }
+
+      // Draw image data to the canvas
+      ctx.putImageData(imageData, 20, 20);
+    }
+    canvas.toBlob((blob) => {
+      downloadImage(blob);
+    }, "image/png");
   };
 
   return (
