6f23476fb9ec0733aba3f185fb44fa8243e44fbd
a2tane <a2tane.hrt@gmail.com>
2022-09-25 23:04:46 +0900

    ファイルダウンロードの実装

--- components/ToolsArea/index.tsx
+++ components/ToolsArea/index.tsx
@@ -2,8 +2,27 @@ import { Download } from "@mui/icons-material";
 import { IconButton } from "@mui/material";
 
 export const ToolsArea = () => {
+  const handleDownload = () => {
+    const text = "TEXT";
+    const blob = new Blob([text], { type: "text/plain" });
+    const url = URL.createObjectURL(blob);
+
+    const anchor = document.createElement("a");
+    anchor.setAttribute("href", url);
+    anchor.setAttribute("download", "FILENAME.txt");
+
+    const mouseEvent = new MouseEvent("click", {
+      bubbles: true,
+      cancelable: true,
+      view: window,
+    });
+
+    anchor.dispatchEvent(mouseEvent);
+    console.log("click");
+  };
+
   return (
-    <IconButton>
+    <IconButton onClick={handleDownload}>
       <Download></Download>
     </IconButton>
   );
